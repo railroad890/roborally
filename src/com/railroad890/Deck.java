47a35050bb5cf80ff4716a9a20f8f4622e0bd0dc
railroad890 <jamestrains890@gmail.com>
2018-06-17 06:16:58 -0400

    added deck class and initial methods

---
+++ src/com/railroad890/Deck.java
@@ -0,0 +1,20 @@
+package com.railroad890;
+
+import java.util.ArrayList;
+
+public class Deck {
+
+    ArrayList<ProgramCard> _deck;
+
+    public Deck(){
+
+    }
+
+    public void shuffle(){
+
+    }
+
+    public void deal(int numPlayers, ArrayList<Integer> numCards){
+
+    }
+}
