519f66abae1268bd6be07328d063af0cb4c637cc
railroad890 <jamestrains890@gmail.com>
2018-06-17 06:10:32 -0400

    added program card variables

--- src/com/railroad890/ProgramCard.java
+++ src/com/railroad890/ProgramCard.java
@@ -1,4 +1,32 @@
 package com.railroad890;
 
 public class ProgramCard {
+
+    private int _priority;
+    private int _type;
+    private int _direction;
+    private int _value;
+
+    public ProgramCard(int priority, int type, int direction, int value){
+        _priority = priority;
+        _type = type;
+        _direction = direction;
+        _value = value;
+    }
+
+    public int get_priority() {
+        return _priority;
+    }
+
+    public int get_type() {
+        return _type;
+    }
+
+    public int get_direction() {
+        return _direction;
+    }
+
+    public int get_value() {
+        return _value;
+    }
 }
